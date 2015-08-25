6f21668fe2c625d677f4c50a29903a3e60482bb4
hd85 <hdossa01@gmail.com>
2015-08-25 18:15:06 -0400

    added the form route

--- crm.rb
+++ crm.rb
@@ -2,20 +2,17 @@ require_relative 'rolodex'
 require_relative 'contact'
 require 'sinatra'
 
+$rolodex = Rolodex.new
+
 get '/' do
   @crm_app_name = "My CRM"
   erb :index
 end
 
 get '/contacts' do
-  @contacts = []
-  @contacts << Contact.new(1000, "Yehuda", "Katz", "yehuda@example.com", "Developer")
-  @contacts << Contact.new(1001, "Mark", "Zuckerberg", "mark@facebook.com", "CEO")
-  @contacts << Contact.new(1002, "Sergey", "Brin", "sergey@google.com", "Co-Founder")
-
   erb :contacts
 end
 
 get '/contacts/new' do
-  "Add a contact"
+  erb :new_contact
 end
