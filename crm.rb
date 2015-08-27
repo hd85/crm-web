4f99e1ceb58f4102914441f714cfa59f22b2520c
hd85 <hdossa01@gmail.com>
2015-08-27 16:34:24 -0400

    added code to edit a contact

--- crm.rb
+++ crm.rb
@@ -44,3 +44,17 @@ get '/contacts/:id/edit' do
     rasie Sinatra::NotFound
   end
 end
+
+put '/contacts/:id' do
+  @contact = $rolodex.find(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.notes = params[:notes]
+
+    redirect to("/contacts")
+  else
+    raise Sinatra::NotFound
+  end
+end
