3ff38951543e40af95bc44339122537e96ef5a2c
hd85 <hdossa01@gmail.com>
2015-08-27 15:47:07 -0400

    adding the show_contact.erb

--- rolodex.rb
+++ rolodex.rb
@@ -6,6 +6,10 @@ class Rolodex
     @id = 1000
   end
 
+  def find(contact_id)
+    @contacts.find {|contact| contact.id == contact_id }
+  end
+
   def add_contact(contact)
     contact.id = @id
     @contacts << contact
