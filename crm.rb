5cfdc11eb4069b79a72528d067e79adfbc572aeb
hd85 <hdossa01@gmail.com>
2015-08-28 15:48:56 -0400

    transformed Contact class into a DataMapper resource

--- crm.rb
+++ crm.rb
@@ -1,9 +1,22 @@
 require_relative 'rolodex'
-require_relative 'contact'
 require 'sinatra'
 require 'data_mapper'
 
 DataMapper.setup(:default, "sqlite3:database.sqlite3")
+
+class Contact
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :first_name, String
+  property :last_name, String
+  property :email, String
+  property :notes, String
+
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
 
 $rolodex = Rolodex.new
 
@@ -46,7 +59,7 @@ get '/contacts/:id/edit' do
   end
 end
 
-put '/contact/:id' do
+put '/contacts/:id' do
   @contact = $rolodex.find(params[:id].to_i)
   if @contact
     @contact.first_name = params[:first_name]
